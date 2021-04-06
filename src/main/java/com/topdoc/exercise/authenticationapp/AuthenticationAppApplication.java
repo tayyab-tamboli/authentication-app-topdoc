6dd62a271bdb8173c24dd91d97f5792d84eaeeba
Tayyab Tamboli <tayyabt120@gmail.com>
2021-04-06 23:33:01 +0530

    INITIAL COMMIT FOR APP SETUP

---
+++ src/main/java/com/topdoc/exercise/authenticationapp/AuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.topdoc.exercise.authenticationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topdoc/exercise/authenticationapp/AuthenticationAppTopdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topdoc.exercise.authenticationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAppTopdocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
